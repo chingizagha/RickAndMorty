70c5cafd27e9130ee035cc8259775781c9cc0d0c
chingizagha <cingiz167@gmail.com>
2024-01-28 14:23:21 +0400

    Update for api call

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -16,11 +16,36 @@ final class RMService {
     /// Privatized constructore
     private init() {}
     
+    enum RMServiceError: Error {
+        case failedToCreateRequest
+        case failedToGetData
+    }
+    
     
     /// Send Rick and Morty API Call
     /// - Parameters:
     ///   - request: Request instance
+    ///   - type: The type of object to expect ot get back
     ///   - completion: Callback data or error
-    public func execute(_ request: RMRequest, completion: @escaping () -> Void) {
+    public func execute<T: Codable>(_ request: RMRequest, expecting type: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        
+        guard let urlRequest = self.request(from: request) else {
+            completion(.failure(RMServiceError.failedToCreateRequest))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            guard let data = data, error == nil else{
+                completion(.failure(error ?? RMServiceError.failedToGetData ))
+                return
+            }
+        }
+    }
+    
+    private func request(from rmRequest: RMRequest) -> URLRequest?{
+        guard let url = rmRequest.url else {return nil}
+        var request = URLRequest(url: url)
+        request.httpMethod = rmRequest.httpMethod
+        return request
     }
 }
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -15,6 +15,9 @@ final class RMCharacterViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(endpoint: .character)
+        print(request.url)
     }
     
 
